70bbfda91990e9748dd9526f4c53df46238cbecf
Farrell Meng <mengfanqi0828@163.com>
2017-03-23 23:29:13 +0800

    迁移vtk_test代码至独立项目Show 3DS。

--- Show 3DS/show_3ds.cpp
+++ Show 3DS/show_3ds.cpp
@@ -13,16 +13,16 @@
 VTK_MODULE_INIT(vtkRenderingOpenGL2);
 VTK_MODULE_INIT(vtkInteractionStyle);
 
-//std::string file_path = "装配散件模型.3DS";
-std::string file_path = "汽缸盖.3DS";
+std::string file_path = "装配散件模型.3DS";
+// std::string file_path = "汽缸盖.3DS";
 
 int main()
 {
-	vtkSmartPointer<vtkRenderWindowInteractor> iren = 
+	vtkSmartPointer<vtkRenderWindowInteractor> iren =
 		vtkSmartPointer<vtkRenderWindowInteractor>::New();	// 创建窗口交互器
-	vtkSmartPointer<vtkRenderWindow> renWin = 
+	vtkSmartPointer<vtkRenderWindow> renWin =
 		vtkSmartPointer<vtkRenderWindow>::New();			// 创建渲染窗口
-	vtkSmartPointer<vtkRenderer> renderer = 
+	vtkSmartPointer<vtkRenderer> renderer =
 		vtkSmartPointer<vtkRenderer>::New();				// 创建渲染器
 
 	iren->SetRenderWindow(renWin);		// 设置交互器控制的窗口为renWin
@@ -31,7 +31,7 @@ int main()
 	renderer->SetBackground(0.1, 0.2, 0.4);			// 设置背景色
 	renWin->SetSize(600, 600);			// 设置窗口的默认大小
 
-	vtkSmartPointer<vtk3DSImporter> importer = 
+	vtkSmartPointer<vtk3DSImporter> importer =
 		vtkSmartPointer<vtk3DSImporter>::New();		// 创建3DS加载器对象
 	importer->SetFileName(file_path.c_str());		// 指定要读取的3DS文件的文件名或路径
 	importer->ComputeNormalsOn();		// 开启法向量计算
@@ -41,7 +41,7 @@ int main()
 	renderer->GetActiveCamera()->SetFocalPoint(100, 0, 50);	// 设置相机的焦点(世界坐标系中)
 	renderer->GetActiveCamera()->SetViewUp(0, 0, 1);		// 设置相机的观察试图
 
-	// 获取加载的3DS模型中物体对象的个数
+															// 获取加载的3DS模型中物体对象的个数
 	int numOfActors = importer->GetRenderer()->GetActors()->GetNumberOfItems();
 
 	// 从3ds图像中依次读取物体
